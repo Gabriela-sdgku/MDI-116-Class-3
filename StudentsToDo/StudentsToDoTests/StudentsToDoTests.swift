2965c577cb0b149a417ce6140287723c19536868
Gabriela-sdgku <gabriela.sanchez@sdgku.edu>
2025-11-24 18:49:14 -0800

    Add files via upload

---
+++ StudentsToDo/StudentsToDoTests/StudentsToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudentsToDoTests.swift
+//  StudentsToDoTests
+//
+//  Created by Gabriela Sanchez on 10/11/25.
+//
+
+import Testing
+@testable import StudentsToDo
+
+struct StudentsToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
